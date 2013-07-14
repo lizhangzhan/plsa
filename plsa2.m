fc3195ebdf2065ecbd989dbca676c97e944915dd
lizhangzhan <lizhangzhan@outlook.com>
2013-07-14 23:45:03 +0800

    two version for plsa implementation

--- plsa2.m
+++ plsa2.m
@@ -1,16 +1,19 @@
-function [prob_term_topic, prob_topic_doc, lls] = plsa(termDocMatrix, numTopic, iter)
+function [prob_term_topic, prob_topic_doc, lls] = plsa2(termDocMatrix, numTopic, iter)
 % Fit a plsa model from a given term-document matrix
 
 [numTerm, numDoc] = size(termDocMatrix);
 
+prob_topic = rand(numTopic, 1); % p(topic)
+prob_topic(:) = sum(prob_topic(:)); % normalization
+
 prob_term_topic = rand(numTerm, numTopic); % p(term | topic)
 for i = 1:numTopic
-	prob_term_topic(:, i) = prob_term_topic(:, i) / sum(prob_term_topic(:, i));
+	prob_term_topic(:, i) = prob_term_topic(:, i) / sum(prob_term_topic(:, i)); % normalization
 end
 
-prob_topic_doc = rand(numTopic, numDoc);   % p(topic | doc)
-for i = 1:numDoc
-	prob_topic_doc(:, i) = prob_topic_doc(:, i) / sum(prob_topic_doc(:, i));
+prob_doc_topic = rand(numDoc, numTopic);   % p(doc | topic)
+for i = 1:numTopic
+	prob_doc_topic(:, i) = prob_doc_topic(:, i) / sum(prob_doc_topic(:, i)); % normalization
 end
 
 prob_topic_term_doc = cell(numTopic, 1);   % p(topic | doc, term)
@@ -31,7 +34,7 @@ for i = 1 : iter
 		w = find(termDocMatrix(:, d));
 		prob_term_doc(w, d) = 0;
 		for z = 1:numTopic
-			prob_topic_term_doc{z}(w, d) = prob_topic_doc(z, d) .* prob_term_topic(w, z);
+			prob_topic_term_doc{z}(w, d) = prob_topic(z) .* prob_doc_topic(d, z) .* prob_term_topic(w, z);
 			prob_term_doc(w, d) = prob_term_doc(w, d) + prob_topic_term_doc{z}(w, d);
 		end
 		for z = 1:numTopic
@@ -40,13 +43,14 @@ for i = 1 : iter
 	end
 	
 	disp('M-step..');
-	disp('Update p(topic | doc)...');
-	for d = 1:numDoc
-		w = find(termDocMatrix(:, d));
-		for z = 1:numTopic
-			prob_topic_doc(z, d) = sum(termDocMatrix(w, d) .* prob_topic_term_doc{z}(w, d));
+	disp('Update p(doc | topic)...');
+	for z = 1:numTopic
+		for d = 1:numDoc
+			w = find(termDocMatrix(:, d));
+			prob_doc_topic(d, z) = sum(termDocMatrix(w, d) .* prob_topic_term_doc{z}(w, d));
 		end
-		prob_topic_doc(:, d) = prob_topic_doc(:, d) / sum(prob_topic_doc(:, d));
+		prob_topic(z) = sum(prob_doc_topic(:, z));
+		prob_topic_doc(:, z) = prob_doc_topic(:, z) / prob_topic(z);
 	end
 	disp('Update p(word | topic)...');
 	for z = 1:numTopic
@@ -54,9 +58,15 @@ for i = 1 : iter
 			d = find(termDocMatrix(w, :));
 			prob_word_topic(w, z) = sum(termDocMatrix(w, d) .* prob_topic_term_doc{z}(w, d));
 		end
+		assert(prob_topic(z) - sum(prob_word_topic(:, z)) < 1e-5);
+		% formatSpec = 'topic %d prob: %f \t %f';
+		% fprintf(formatSpec, prob_topic(z), sum(prob_word_topic(:, z)));
 		prob_word_topic(:, z) = prob_word_topic(:,z) / sum(prob_word_topic(:,z));
 	end
 	
+	disp('Update p(topic)...');
+	prob_topic(:) = prob_topic(:) / sum(prob_topic(:));
+
 	% calculate likelihood
 	fprintf('Iteration %d\n', i);
 	disp('Calculate maximum likelihood...');
--- run.m
+++ run.m
@@ -3,7 +3,7 @@ global MAXNUMDIM;
 MAXNUMDIM = 20000; % global variable, dimension of terms
 global MAXNUMDOC;
 MAXNUMDOC = 2000;  % global variable, number of documents
-numTopic = 16;     % number of topics
+numTopic = 10;     % number of topics
 numIter = 50;      % number of iteration
 
 % 1th, preprocess the raw text set
@@ -13,7 +13,7 @@ fprintf('Num of dimension: %d\n', size(termDocMatrix, 1));
 fprintf('Num of document: %d\n', size(termDocMatrix, 2));
 
 % 2th, fit a plsa model from a given term-doc matrix
-[prob_term_topic, prob_topic_doc, lls] = plsa(termDocMatrix, numTopic, numIter);
+[prob_term_topic, prob_topic_doc, lls] = plsa2(termDocMatrix, numTopic, numIter);
 
 % 3th, display topN keywords for each topic
 topN = 50;
