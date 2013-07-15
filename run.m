e13ff550a81d6ce4378e583c8f2e22d437bef3fa
lizhang <lizhangzhan@outlook.com>
2013-07-15 11:01:41 +0800

    Update run.m

--- run.m
+++ run.m
@@ -13,7 +13,8 @@ fprintf('Num of dimension: %d\n', size(termDocMatrix, 1));
 fprintf('Num of document: %d\n', size(termDocMatrix, 2));
 
 % 2th, fit a plsa model from a given term-doc matrix
-[prob_term_topic, prob_topic_doc, lls] = plsa2(termDocMatrix, numTopic, numIter);
+% [prob_term_topic, prob_topic_doc, lls] = plsa(termDocMatrix, numTopic, numIter);
+[prob_term_topic, prob_doc_topic, prob_topic, lls] = plsa2(termDocMatrix, numTopic, numIter);
 
 % 3th, display topN keywords for each topic
 topN = 50;
